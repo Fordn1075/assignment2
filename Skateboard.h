d74ba163926be0143d974f22c3fab1572d8ca2b9
Nicholas Ford <fordn1075@gmail.com>
2019-10-04 15:54:23 -0400

    Added Skateboard.h - Changed Jet.cpp to add time to mileage

---
+++ Skateboard.h
@@ -0,0 +1,15 @@
+//
+//Nicholas Ford 10/04/19
+//
+
+#ifndef DRIVINGSIMULATOR_SKATEBOARD_H
+#define DRIVINGSIMULATOR_SKATEBOARD_H
+
+class Skateboard : public Vehicle 
+{
+public:
+	virtual double mileageEstimate(double time);
+};
+
+
+#endif //DRIVINGSIMULATOR_SKATEBOARD_H
