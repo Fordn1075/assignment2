e93dab6fb677d9b248fb3ccf283c2e626f55071f
Nicholas Ford <fordn1075@gmail.com>
2019-10-04 20:20:06 -0400

    Added Train.h

---
+++ Train.h
@@ -0,0 +1,20 @@
+//
+//Created by Nicholas Ford 10/03/19
+//
+
+#ifndef DRIVINGSIMULATOR_TRAIN_H
+#define DRIVINGSIMULATOR_TRAIN_H
+
+#include "PoweredVehicle.h"
+
+class Train : public PoweredVehicle{
+
+private:
+	int numberOfCoalNuggets;
+
+public:
+	Train();
+	virtual double mileageEstimate(double time);
+};
+
+#endif //DRIVINGSIMULATOR_TRAIN_H
